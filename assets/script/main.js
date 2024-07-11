3185a3356a8d532c222eeee47f0a394b1b706184
Thalles José <thallesjosesv@gmail.com>
2024-07-11 12:00:58 -0300

    main

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,16 +2,23 @@ document.addEventListener('DOMContentLoaded', function(){
     const apiKey = ""
     const seletorMoeda = document.getElementById("coin")
     const result = document.getElementById("result")
+    const selectSpan = document.getElementById("selectSpan")
 
     seletorMoeda.addEventListener('change', function(){
         const seletorValue = seletorMoeda.value;
 
-        if (seletorValue === 'dollar'){
-            result.innerHTML = ' ok'
+        if (seletorValue === 'dollar')
+        {
+            console.log("lindo")
+        }
+        else
+        {
+            console.log("feio")
         }
 
 
     })
+
 
 
 });
